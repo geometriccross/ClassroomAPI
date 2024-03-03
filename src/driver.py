4ed09632bfc905df2ba42f4f8aaaac724aad10f8
geometriccross <geometriccross@gmail.com>
2024-03-04 08:06:03 +0900

    googleへのログインの関数をより簡潔に

--- src/driver.py
+++ src/driver.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 username = str
 password = str
@@ -24,30 +25,32 @@ def create_webdriver(driver_path: PurePath, driver_arguments: List[str]):
     # ブラウザを開く
     return webdriver.Chrome(service=service, options=options)
 
-def wait_for_element(driver, by, value, timeout=10):
-    """指定された要素が現れるまで待機し、その要素を返すヘルパー関数"""
-    return WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((by, value))
-    )
+def wait_for_element(driver: webdriver, by: By, value: str, timeout: int = 10):
+    try:
+        return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, value)))
+    except TimeoutException:
+        print(f"Error: Timeout waiting for element by {by} with value {value}")
+        return None
 
 def login_to_google_classroom(driver: webdriver, username: username, password: password):
+    """
+    Google Takeoutにアクセスしてログインします。
+    Googleアカウントのユーザー名とパスワードを使用して、Google Takeoutにログインします。
+    """
+
     driver.get("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Ftakeout.google.com%2F%3Fhl%3Dja&followup=https%3A%2F%2Ftakeout.google.com%2F%3Fhl%3Dja&hl=ja&ifkv=ATuJsjzLD6xO4Hk_ZGE3sE9VhY6ACjrc0naKIJQ6DUlMF4eTd7LAnFfft-R23r6VqUjo71Wx7IJo4w&osid=1&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1728425499%3A1709466484511131&theme=glif")
     
     # ログインフォームを探す
-    username_input = wait_for_element(driver, By.ID, "identifierId")
-    username_input.send_keys(username + Keys.RETURN)
-
-    # 続行ボタンを押す
-    wait_for_element(driver, By.ID, "identifierNext").click()
+    if username_input := wait_for_element(driver, By.ID, "identifierId"):
+        username_input.send_keys(username)
+        wait_for_element(driver, By.ID, "identifierNext").click()
     
     # パスワード入力
-    password_input = wait_for_element(driver, By.NAME, "password")
-    password_input.send_keys(password + Keys.RETURN)
+    if password_input := wait_for_element(driver, By.CSS_SELECTOR, "#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"):
+        password_input.send_keys(password)
+        wait_for_element(driver, By.ID, "passwordNext").click()
 
-    # 続行ボタンを押す
-    wait_for_element(driver, By.ID, "identifierNext").click()
-
-    # ログイン後のページが読み込まれるまで待機
-    wait_for_element(driver, By.CLASS_NAME, "xF2V0d")
+    if easy_to_login := wait_for_element(driver, By.XPATH, "//div[@jsname='eBSUOb']", 2):
+        easy_to_login.click()
 
     return driver
--- test/test_driver.py
+++ test/test_driver.py
@@ -1,14 +1,20 @@
-import unittest
-from unittest.mock import MagicMock, patch
+import os
+import pytest
+from pathlib import Path
 
 from selenium import webdriver
-
+from selenium.common.exceptions import TimeoutException
+from dotenv import load_dotenv
 from src.driver import login_to_google_classroom
 
-class TestLoginToGoogleClassroom(unittest.TestCase):
-    def test_login_to_google_classroom(self):
+def test_login_to_google_classroom():
+    load_dotenv()
+    FOR_TEST_USERNAME = os.environ["FOR_TEST_USERNAME"]
+    FOR_TEST_EMAIL = os.environ["FOR_TEST_EMAIL"]
+    FOR_TEST_PASSWORD = os.environ["FOR_TEST_PASSWORD"]
+    
+    try:
         driver = webdriver.Chrome()
-        login_to_google_classroom(driver, 'test_username', 'test_password')
-
-if __name__ == '__main__':
-    unittest.main()
+        login_to_google_classroom(driver, FOR_TEST_EMAIL, FOR_TEST_PASSWORD)
+    except TimeoutException:
+        assert False, "TimeoutException occurred during login"

927ae56d12a0c8faf74173ae0bc8e4c8755a1529
Xycydg5150 <geometriccross@gmail.com>
2024-04-14 20:23:13 +0900

    ドライバを削除する関数を追加

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 from time import time
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, status
 from selenium import webdriver
+from shutil import rmtree
 
 from src import driver
 
@@ -30,6 +30,28 @@ async def add_driver():
     global drivers
     drivers.append(instance_gen.__next__())
     return {"message": "new driver is added"}
+
+@app.delete("/remove_driver/{driver_index}", status_code=status.HTTP_200_OK)
+async def remove_driver(driver_index: int):
+    global drivers
+    try:
+        driver_to_remove = drivers.pop(driver_index)
+        driver_to_remove.quit()
+        rmtree(driver_to_remove.capabilities['chrome']['userDataDir'])
+        return {"message": f"Driver at index {driver_index} has been removed"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f"No driver found at index {driver_index}")
+
+@app.delete("/clear_driver", status_code=status.HTTP_204_NO_CONTENT)
+async def clear_driver():
+    global drivers
+    if len(drivers) > 0:
+        for driver in drivers:
+            driver.quit()
+            rmtree(driver.capabilities['chrome']['userDataDir'])
+        drivers.clear()
+    else:
+        raise HTTPException(status_code=404, detail="No driver to clear")
 
 # アプリケーションの終了時にドライバーを終了
 @app.on_event("shutdown")

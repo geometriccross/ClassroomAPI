e80344ea26842c83ca57a8ad018c31d1ce1a6757
Xycydg5150 <geometriccross@gmail.com>
2024-06-25 00:32:45 +0900

    split a module

--- tests/unit/services/test_scraping.py
+++ tests/unit/services/test_scraping.py
@@ -3,13 +3,10 @@ import shutil
 from pathlib import Path
 from time import time
 
-import pytest
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-from src.services.driver import generate_driver_instances, webdriver_profile_generator
 from src.services.scraping import page_objects as po
 from src.services.scraping.permission_passing import Credentials
 
@@ -26,36 +23,6 @@ LOGIN_URL = get_env("LOGIN_URL")
 SECTION_TEST_URL = get_env("SECTION_TEST_URL")
 COURSES_TEST_URL = get_env("COURSES_TEST_URL")
 FILES_TEST_URL = get_env("FILES_TEST_URL")
-
-
-@pytest.fixture
-def cred() -> Credentials:
-    load_dotenv(".env")
-
-    # 環境変数からログイン情報を取得
-    return Credentials(
-        email=get_env("COLLAGE_EMAIL"),
-        name=get_env("COLLAGE_USERNAME"),
-        password=get_env("COLLAGE_PASSWORD"),
-    )
-
-
-@pytest.fixture
-def test_driver(cred: Credentials):
-    test_dir = Path("test/chrome_drivers")
-    test_dir.mkdir(parents=True, exist_ok=True)
-
-    driver_generator = generate_driver_instances(
-        profile_gen=webdriver_profile_generator(test_dir),
-        driver_arguments=["--headless=new"],
-        cred=cred,
-    )
-
-    driver = next(driver_generator)
-    yield driver
-
-    driver.quit()
-    shutil.rmtree(test_dir)
 
 
 def test_login(cred: Credentials):
